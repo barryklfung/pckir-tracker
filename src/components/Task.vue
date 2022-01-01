72638c9b94a226afcf8071c97194c85f8989fc32
K. L. Barry Fung <barry@klfung.ca>
2022-01-01 15:19:23 -0800

    setting update and accessibility functions figuring out completion update still

--- src/components/Task.vue
+++ src/components/Task.vue
@@ -1,41 +1,43 @@
 <script setup>
-import { ref } from 'vue'
+import { ref } from "vue";
 
 defineProps({
+  nodeId: Number,
   name: String,
-  isCompleted: Boolean
-})
-
-const count = ref(0)
+  isCompleted: Boolean,
+  prerequisiteNodeIds: Array, //Array of NodeIds;
+  dependentNodeIds: Array, //Array of NodeIds;
+  prerequisitesAchieved: Array, //Array of Booleans
+});
+export default {
+  computed: {
+    //checking accessibility should be fast:
+    isAccessible() {
+      // if there are dependencies
+      if (this.prerequisiteNodeIds.length > 0)
+        // return 1 if nothing is _unsatisfied_
+        return this.prerequisitesAchieved.includes(false) ? false : true;
+      else return true; // return 1 otherwise.
+    },
+  },
+  watch: {
+    isCompleted(newState, oldState) {
+      // if we complete a task, update all dependencies.
+      //Not sure right now, pass both new state, nodeId, and dependentIds, up, probably?
+    },
+  },
+  methods: {
+    //nodeId: number, dependency that was toggled
+    //state: Boolean, new state.
+    updateDependency(nodeId, state) {
+      // Update the input prereq (should work for completed vs not)
+      const index = this.prerequisitesNodeIds.indexOf(nodeId);
+      if (index > -1) this.prerequisitesAchieved[index] = state;
+    },
+  },
+};
 </script>
 
-<template>
-  <h1>{{ msg }}</h1>
+<template></template>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
-</template>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
+<style scoped></style>
