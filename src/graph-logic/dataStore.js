ce4e724d66b55f88caaeb74a98b26903f16d5e01
K. L. Barry Fung <barry@klfung.ca>
2022-01-01 18:45:47 -0800

    adding tiny-emitter
    Using this so that I don't have to expose the _entire_ store for each component, just the subscribe

--- src/graph-logic/dataStore.js
+++ src/graph-logic/dataStore.js
@@ -10,20 +10,24 @@
 //DESIGN store based on this format:
 
 import primTask from "./primTask.js";
-
+var emitter = require("tiny-emitter/instance");
 export const store = {
   state: {
     nodeArray, //Array of nodes shown above
     numNodes, //Explicit variable.
     idToName, //hash map
     nameToId, //hash map
+    roots, //array
   },
 
   sendUpdate(senderID, state, nodesTobeUpdated) {
     for (let i = 0; i < nodesTobeUpdated.length; i++) {
-      if (i < state.numNodes)
-        state.nodeArray[i].updateDependency(senderID, state);
-      else console.log("Attempted to access nodeArray out of index");
+      if (i < state.numNodes) {
+        var old_state = state.nodeArray[i].isAccessible();
+        //Update. If state is toggled, update UI.
+        if (old_state != state.nodeArray[i].updateDependency(senderID, state))
+          this.updateUIStoreChange(i);
+      } else console.log("Attempted to access nodeArray out of index");
     }
   },
 
@@ -39,21 +43,28 @@ export const store = {
     state.nodeArray = new Array();
     state.idToName = {};
     state.nameToId = {};
+    state.roots = new Array();
+    state.map = new Array();
     //Populate Node Array
     for (let i = 0; i < nodes.length; i++) {
-      state.idToName[i] = nodes.name;
-      state.nameToId[nodes.name] = i;
-      //    constructor(nodeId, name, prerequisiteNodeIds, dependentNodeIds) %initialize with a full array of -1, or n ull if no prereqs. Dependencies are populated later.
+      state.idToName[i] = nodes[i].name;
+      state.nameToId[nodes[i].name] = i;
+      //    constructor(nodeId, name, type, parent, prerequisiteNodeIds, dependentNodeIds) %initialize with a full array of -1, or n ull if no prereqs. Dependencies are populated later.
       state.nodeArray.push(
         new primTask(
           i,
           nodes[i].name,
+          nodes[i].type,
+          nodes[i].parent,
           nodes[i].prerequisites.length > 0
             ? new Array(nodes[i].prerequisites.length).fill(-1)
             : null,
           []
         )
       );
+      if (nodes[i].prerequisites == null && nodes[i].type != "key_item") {
+        state.roots.push(i);
+      }
     }
     state.numNodes = nodeArray.length; //Yes, this is dumb.
     //Populate dependnecies, and do the name to ID conversion.
@@ -67,6 +78,15 @@ export const store = {
       }
     }
   },
+
+  //For components to access
+  subscribeStoreChange(nodeId, update_function) {
+    emitter.on(idToName[nodeId], update_function);
+  },
+  updateUIStoreChange(nodeId) {
+    emitter.emit(idToName[nodeId]);
+  },
+
   resetState() {
     //JAVASCRIPT I DON'T GET GARBAGE COLLECTION AND I WANT TO FORCE EMPTY.
     state.nodeArray = null;
